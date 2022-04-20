1a7053a94b663614e22c4e7b1c2d18832f3ce090
SIDDU <SIDDU@192.168.1.35>
2022-04-20 23:10:41 +0530

    # WARNING: head commit changed in the meantime This is my first commit

---
+++ src/SimpleProg.java
@@ -0,0 +1,18 @@
+
+public class SimpleProg {
+
+	public static void main(String[] args) {
+	 
+		String str = "Sridhar";
+		if(str == "Sridhar1") {
+			System.out.println("It is not equal");
+		}
+		else 
+			{
+				System.out.println("its equal");
+			}
+		}
+
+	}
+
+
